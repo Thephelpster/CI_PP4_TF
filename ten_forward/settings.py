8432a70c0baacc8d696dca7cb158b29f44eefe6b
jamie <thephelpster1992@gmail.com>
2024-01-22 13:41:08 +0000

    fix requirements backports.zoneinfo issue

--- ten_forward/settings.py
+++ ten_forward/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*',
-                 '.herokuapp.com']
+                 'https://ten-forward-53badab24b10.herokuapp.com']
 
 
 # Application definition
