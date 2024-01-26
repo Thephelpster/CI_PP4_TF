f194751101689369456541d305e159132b5904b7
jamie <thephelpster1992@gmail.com>
2024-01-26 17:17:29 +0000

    add menu.html and adjust views and urls

--- menus/urls.py
+++ menus/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from menus import views
 
 urlpatterns = [
+    path('', views.menu, name='menu'),
     path('food_menu', views.food_menu, name='food_menu'),
+    path('drink_menu', views.drink_menu, name='drink_menu'),
 ]
--- menus/views.py
+++ menus/views.py
@@ -4,7 +4,15 @@ from .models import Food
 
 # Create your views here.
 
+def menu(request):
+    return render(request, 'menu/menu.html')
+
 def food_menu(request):
     food_list = Food.objects.all()
     return render(
         request, 'menus/food_menu.html', {'food_list': food_list})
+
+def drink_menu(request):
+    drink_list = DrinkItem.objects.all()
+    return render(
+        request, 'menus/drink_menu.html', {'drink_list': drink_list})
