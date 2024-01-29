aa049a2ea7eff78e0a317ba3efce3be25b65eee1
jamie <thephelpster1992@gmail.com>
2024-01-29 15:26:31 +0000

    add sign up function and page

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('sign_up/', views.register_user, name='sign_up'),
 ]
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,redirect
 from django.contrib import messages 
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def login_user(request):
@@ -18,3 +19,25 @@ def login_user(request):
 			return redirect('login')
 	else:
 		return render(request, 'authenticate/login.html', {})
+	
+def logout_user(request):
+	logout(request)
+	messages.success(request, ('You Have Been Logged Out...'))
+	return redirect('home')
+
+def register_user(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ('You Have Registered...'))
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	
+	context = {'form': form}
+	return render(request, 'authenticate/sign_up.html', context)
