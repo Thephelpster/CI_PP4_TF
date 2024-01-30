87a1520a22f23134373ba45e0389cc08bf38b8be
jamie <thephelpster1992@gmail.com>
2024-01-30 09:51:17 +0000

    add forms for sign up page

--- authenticate/views.py
+++ authenticate/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render ,redirect
 from django.contrib import messages 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 # Create your views here.
 
 def login_user(request):
@@ -27,7 +28,7 @@ def logout_user(request):
 
 def register_user(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = SignUpForm(request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data['username']
@@ -37,7 +38,7 @@ def register_user(request):
 			messages.success(request, ('You Have Registered...'))
 			return redirect('home')
 	else:
-		form = UserCreationForm()
+		form = SignUpForm()
 	
 	context = {'form': form}
 	return render(request, 'authenticate/sign_up.html', context)
